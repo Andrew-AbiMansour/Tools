e4fdb0ade2234c9d7e71b08eccf3e61cf9c2068e
Andrew-AbiMansour <anabiman@indiana.edu>
2013-02-03 02:13:13 -0500

    Update PyPlot.py

--- PyPlot.py
+++ PyPlot.py
@@ -2,7 +2,7 @@ from matplotlib.pyplot import figure, show
 import numpy as np
 from matplotlib.image import NonUniformImage
 
-def pcolor(z,y=np.arange(10),x=np.arange(10),interp='bilinear')
+def pcolor(z, y = np.arange(10), x = np.arange(10), interp = 'bilinear')
   '''
   z = np.random.rand(10,10)
   '''
